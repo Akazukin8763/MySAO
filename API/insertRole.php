7e27460d2bc1a99b5b6f95a2c2dba7b25192ea2d
ubin0914 <1213121abc@gmail.com>
2021-12-23 21:29:38 +0800

    沒用

--- API/insertRole.php
+++ API/insertRole.php
@@ -17,37 +17,52 @@
     }
 
     if ($_SERVER['REQUEST_METHOD'] == "POST") {
-        foreach($_POST as $key => $value){
-            $$key = $value;
-        }
-        /*
         $id = $_POST["id"];
         $name = $_POST["name"];
-        
         $description = $_POST["description"];
         $levels = $_POST["levels"];
         $guild_ID = $_POST["guild_ID"];
-        $health = $_POST["health"];
         $attack = $_POST["attack"];
         $defense = $_POST["defense"];
         $reaction = $_POST["reaction"];
-        $agile = $_POST["agile"];*/
+        $agile = $_POST["agile"];
 
         if ($id == null) exit;
         if ($name == null) exit;/*
         if ($description == null) $description = "";
-        if ($levels == null) $levels = "";*/
-        if ($guild_ID == null) $guild_ID = NULL;/*
+        if ($levels == null) $levels = "";
+        if ($guild_ID == null) $guild_ID = "";*/
+        /*
         if ($health == null) exit;
         if ($attack == null) exit;
         if ($defense == null) exit;
         if ($reaction == null) exit;
         if ($agile == null) exit;*/
 
-        $sql = "INSERT INTO player(ID, name)
-                VALUES(?, ?)";
+        //$sql = "INSERT INTO ability VALUES($id, $health, $attack, $defense, $reaction, $agile)";
+        $sql = "INSERT INTO player VALUES($id, $name, NULL, NULL, NULL)";
         $stmt = $conn->prepare($sql);
+        //$stmt->execute(array($id, $health, $attack, $defense, $reaction, $agile));
         $stmt->execute(array($id, $name));
+        $result = $stmt->fetchAll();
+        
+        echo "??";
+        if (count($result) != 0) {
+            $data = array();
+
+            for ($i = 0; $i < count($result); $i++) {
+                $p = new player();
+
+                $p->ds = $result[$i]["description"];
+
+                $data[] = $p;
+            }
+            
+            echo json_encode(array('result' => $data));
+        }
+        else {
+            echo json_encode(array('errorMsg' => 'STARBURST STREAM'));
+        }
     }
     else {
         echo json_encode(array('errorMsg' => '請求無效，只允許 POST 方式訪問！'));
