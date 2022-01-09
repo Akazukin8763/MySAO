330ddd2ec0914bc3556e67b4e0c7467b48e4689c
Akazukin8763 <starmoon900709@gmail.com>
2022-01-09 11:34:43 +0800

    更新 Main main：連結至各區塊

--- main.php
+++ main.php
@@ -24,6 +24,9 @@
         <!-- Bootstrap CSS --><!-- CSS only -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
+        <link href="css/bootstrap-col-13.css" rel="stylesheet">
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
@@ -43,6 +46,34 @@
                 background: -webkit-linear-gradient(270deg, rgba(16, 46, 102, 0.9)0%, rgba(135, 220, 233, 0.8)100%);
                 background: -o-linear-gradient(270deg, rgba(16, 46, 102, 0.9)0%, rgba(135, 220, 233, 0.8)100%);
             }
+
+            h2 {
+                color: white;
+                font-family: "Lucida Console", Courier, monospace;
+                -webkit-text-stroke: 1.2px black;
+            }
+
+            .card {
+                transition: .3s transform cubic-bezier(.155,1.105,.295,1.12),.3s box-shadow,.3s -webkit-transform cubic-bezier(.155,1.105,.295,1.12);
+                cursor: pointer;
+            }
+            .card:hover {
+                transform: scale(1.05);
+                box-shadow: 0 10px 20px rgba(0,0,0,.12), 0 4px 8px rgba(0,0,0,.06);
+            }
+            .card-image {
+                position: relative;
+                text-align: center;
+            }
+            .card-image img {
+                opacity: 50%;
+            }
+            .card-image div {
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+            }
         </style>
 
         <script type="module">
@@ -60,6 +91,7 @@
     </head>
     <body>
         <div class="Aincrad">
+
             <nav class="navbar navbar-expand-lg navbar-dark bg-dark header-gradient">
                 <div class="container-fluid">
                     <a class="navbar-brand" href="#">
@@ -96,6 +128,63 @@
                     </div>
                 </div>
             </nav>
+
+            <div class="container">
+                <br>
+                <div class="d-flex justify-content-center">
+                    <h2>Welcome to Sword Art Online !</h2>
+                </div>
+                    
+                <br>
+                <div class="row">
+                    <div class="col-sm-6-13">
+                        <div class="card" style="height: 34vh">
+                            <div class="card-image" onclick="location.href='status.php'">
+                                <img src="src/image/Main-Status.png" class="card-img-top" style="height: 33.7vh;">
+                                <div>
+                                    <h2>Status</h2>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div style="height: 8vh"></div>
+
+                        <div class="card" style="height: 34vh">
+                            <div class="card-image" onclick="location.href='guild.php'">
+                                <img src="src/image/Main-Guild.png" class="card-img-top" style="height: 33.7vh;">
+                                <div>
+                                    <h2>Guild</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-1-13"></div>
+
+                    <div class="col-sm-6-13">
+                        <div class="card" style="height: 34vh">
+                            <div class="card-image" onclick="location.href='aincrad.php'">
+                                <img src="src/image/Main-Aincrad.png" class="card-img-top" style="height: 33.7vh;">
+                                <div>
+                                    <h2>Aincrad</h2>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div style="height: 8vh"></div>
+
+                        <div class="card" style="height: 34vh">
+                            <div class="card-image" onclick="location.href='player.php'">
+                                <img src="src/image/Main-Player.png" class="card-img-top" style="height: 33.7vh;">
+                                <div>
+                                    <h2>Player</h2>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
         </div>
     </body>
 </html>
